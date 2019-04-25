8feeda3eb0951cf474959732860e1f53798a3ad6
Anzhelika Kuzmnko <akuzme@softserveinc.com>
2019-04-25 16:23:27 +0300

    Edited code according to Google Java Style Guide

--- src/Student.java
+++ src/Student.java
@@ -1,66 +1,65 @@
 public class Student {
-    private int rating;
-    private String name;
-    static int countOfStudent;
-    static double summaryRating;
+  private int rating;
+  private String name;
+  static int countOfStudent;
+  static double summaryRating;
 
-    // TODO implement Student class according to the instructions providede in the README.md file
+  // TODO implement Student class according to the instructions providede in the README.md file
 
-    public Student(String name) {
-        this.name = name;
-        countOfStudent++;
-    }
+  public Student(String name) {
+    this.name = name;
+    countOfStudent++;
+  }
 
-    public static double getAvgRating() {
-        // TODO return average rating of all students
-        if (countOfStudent > 0) {
-            return summaryRating / countOfStudent;
-        } else return 0;
-    }
+  public static double getAvgRating() {
+    // TODO return average rating of all students
+    if (countOfStudent > 0) {
+      return summaryRating / countOfStudent;
+    } else return 0;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        // TODO set student's name
-        this.name = name;
-    }
+  public void setName(String name) {
+    // TODO set student's name
+    this.name = name;
+  }
 
-    public int getRating() {
-        return rating;
-    }
+  public int getRating() {
+    return rating;
+  }
 
-    public void setRating(int rating) {
-        // TODO initialize rating
-        this.rating = rating;
-        summaryRating = summaryRating + this.rating;
-    }
+  public void setRating(int rating) {
+    // TODO initialize rating
+    this.rating = rating;
+    summaryRating = summaryRating + this.rating;
+  }
 
+  public boolean betterStudent(Student student) {
+    // TODO return the result of comparing this.student's rating with student's rating
+    return getRating() > student.getRating();
+    // return this.rating > student.rating;
+  }
 
-    public boolean betterStudent(Student student) {
-        // TODO return the result of comparing this.student's rating with student's rating
-        return getRating() > student.getRating();
-        //return this.rating > student.rating;
-    }
+  public void changeRating(int rating) {
+    // TODO change this student's rating and average raiting of all students
+    double a = getRating();
+    summaryRating = summaryRating - a;
+    this.setRating(rating);
+  }
 
-    public void changeRating(int rating) {
-        // TODO change this student's rating and average raiting of all students
-        double a = getRating();
-        summaryRating = summaryRating - a;
-        this.setRating(rating);
-    }
+  public static void removeStudent(Student student) {
+    // TODO remove student = count - rating from summaryRating
+    countOfStudent--;
+    summaryRating = summaryRating - student.rating;
+    getAvgRating();
+  }
 
-    public static void removeStudent(Student student) {
-        // TODO remove student = count - rating from summaryRating
-        countOfStudent--;
-        summaryRating = summaryRating - student.rating;
-        getAvgRating();
-    }
-
-    @Override
-    public String toString() {
-        // TODO return String with name and rating of this student
-        return "name and rating: " + getName() + getRating();
-    }
+  @Override
+  public String toString() {
+    // TODO return String with name and rating of this student
+    return "name and rating: " + getName() + getRating();
+  }
 }
